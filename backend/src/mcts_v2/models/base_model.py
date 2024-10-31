6f73b861529cc4840cc88f0b02ec754f243b6498
梁盼 <2640279309@qq.com>
2024-10-31 15:42:31 +0800

    删除多余代码

--- backend/src/mcts_v2/models/base_model.py
+++
@@ -1,7 +0,0 @@
-class BaseModel:
-    def __init__(self, model_name, instruction):
-        self.model_name = model_name
-        self.instruction = instruction
-
-    def generate_response(self, conversation):
-        raise NotImplementedError("子类需要实现 generate_response 方法")
--- backend/src/mcts_v2/utils/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def read_instruction(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        instruction = f.read()
-    return instruction
-
-def format_conversation(conversation):
-    return "\n".join([f"{msg['role']}: {msg['content']}" for msg in conversation])
